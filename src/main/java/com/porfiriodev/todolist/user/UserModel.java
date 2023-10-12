3063aa7e9dce94c0049edcf49d2dbf98861b6fdb
lukeskw <lucasporfirioa@gmail.com>
2023-10-12 17:06:36 -0300

    second lesson completed 🔥

--- src/main/java/com/porfiriodev/todolist/user/UserModel.java
+++ src/main/java/com/porfiriodev/todolist/user/UserModel.java
@@ -1,33 +1,29 @@
 package com.porfiriodev.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name="users")
 public class UserModel {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
 
